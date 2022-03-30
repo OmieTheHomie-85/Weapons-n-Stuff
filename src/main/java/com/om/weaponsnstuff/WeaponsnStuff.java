37c0072fa46b655795244f1f45917ec3b795c834
Om Chauhan <skyarmy0170@gmail.com>
2022-03-29 22:12:42 -0400

    Initial commit

---
+++ src/main/java/com/om/weaponsnstuff/WeaponsnStuff.java
@@ -0,0 +1,33 @@
+package com.om.weaponsnstuff;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(WeaponsnStuff.MOD_ID)
+public class WeaponsnStuff {
+    public static final String MOD_ID = "weaponsnstuff";
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public WeaponsnStuff() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+}
+
