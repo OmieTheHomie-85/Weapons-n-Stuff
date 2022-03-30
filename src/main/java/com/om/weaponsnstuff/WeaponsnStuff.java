4635297975ff86ff0e04e1ff70f97c9bd6509da7
Om Chauhan <skyarmy0170@gmail.com>
2022-03-29 22:53:59 -0400

    New COde lol (item add)

--- src/main/java/com/om/weaponsnstuff/WeaponsnStuff.java
+++ src/main/java/com/om/weaponsnstuff/WeaponsnStuff.java
@@ -1,7 +1,9 @@
 package com.om.weaponsnstuff;
 
+import com.om.weaponsnstuff.item.ModItems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -16,8 +18,12 @@ public class WeaponsnStuff {
     private static final Logger LOGGER = LogManager.getLogger();
 
     public WeaponsnStuff() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/com/om/weaponsnstuff/item/ModItems.java
@@ -0,0 +1,26 @@
+package com.om.weaponsnstuff.item;
+
+import com.om.weaponsnstuff.WeaponsnStuff;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, WeaponsnStuff.MOD_ID);
+
+    public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> RAW_CITRINE = ITEMS.register("raw_citrine",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
